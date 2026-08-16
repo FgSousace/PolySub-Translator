b98641baabb8ae302c26ddebc07b078a86e8e4c2
FgSousace <126411487+FgSousace@users.noreply.github.com>
2026-08-16 23:37:12 +0200

    Set app version to 0.6.0

--- src/polysub/__init__.py
+++ src/polysub/__init__.py
@@ -7,4 +7,4 @@ Licensed for noncommercial use only under PolyForm Noncommercial 1.0.0.
 from .models import TranslationMode
 
 __all__ = ["TranslationMode"]
-__version__ = "0.5.10"
+__version__ = "0.6.0"
